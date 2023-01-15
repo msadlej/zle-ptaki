131c284a400e28877271c6d947894ff4b77f38aa
msadlej <01178587@pw.edu.pl>
2023-01-15 18:08:36 +0100

    .

--- test_level.py
+++ test_level.py
@@ -1,17 +1,15 @@
 from level import Level, IvalidAttemptsError
-from PySide2.QtWidgets import QMainWindow
 import pytest
 
 
 def test_level_init():
-    window = QMainWindow()
-    level = Level(1, [], window)
+    level = Level(1, [], "window")
     assert level.attempts == 1
     assert level.targets == []
     assert level.result is False
-    assert level.window is window
+    assert level.window == "window"
 
 
 def test_level_init_error():
     with pytest.raises(IvalidAttemptsError):
-        Level(0, [], QMainWindow())
+        Level(0, [], "window")
